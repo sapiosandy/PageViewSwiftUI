638b9839356b8a091a1d20f755a7e541217d08be
Sandy Gomez <sgomez1029@gmail.com>
2024-05-09 20:10:41 -0700

    Creating Page Views in Swift

--- PageViewSwiftUI/ContentView.swift
+++ PageViewSwiftUI/ContentView.swift
@@ -6,19 +6,67 @@
 //
 
 import SwiftUI
+class Broadcast: ObservableObject {
+    @Published var message = "Shared text"
+}
 
 struct ContentView: View {
+    @ObservedObject var shareMe = Broadcast()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            FirstView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("First tab")
+                }
+                SecondView()
+                    .tabItem {
+                        Image(systemName: "car.fill")
+                        Text("Second Tab")
+                    }
+                ThirdView()
+                    .tabItem {
+                        Image(systemName: "hare.fill")
+                        Text("Third Tab")
+                    }
+            }.environmentObject(shareMe)
+                .tabViewStyle(PageTabViewStyle())
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
         }
-        .padding()
+    }
+    struct FirstView: View {
+        @EnvironmentObject var junkHere: Broadcast
+        var body: some View {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.yellow)
+                Text("First view + \(junkHere.message)")
+            }
+        }
+    }
+    
+struct SecondView: View {
+    @EnvironmentObject var stuffHere: Broadcast
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.purple)
+            Text("Second view + \(stuffHere.message)")
+        }
     }
 }
-
-#Preview {
-    ContentView()
-}
+    
+    struct ThirdView: View {
+        @EnvironmentObject var anotherString: Broadcast
+        var body: some View {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.red)
+                TextField("Type here", text: $anotherString.message)
+            }
+        }
+    }
+    
+    #Preview {
+        ContentView()
+    }
