33d2ff1b93338ef4a3eba5ae0c4545df86a650c4
Sandy Gomez <sgomez1029@gmail.com>
2024-05-09 19:37:20 -0700

    Initial Commit

---
+++ PageViewSwiftUI/PageViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageViewSwiftUIApp.swift
+//  PageViewSwiftUI
+//
+//  Created by Sandra Gomez on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PageViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
